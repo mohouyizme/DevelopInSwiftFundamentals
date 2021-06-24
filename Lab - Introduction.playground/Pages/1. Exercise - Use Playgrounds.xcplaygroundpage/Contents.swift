25da8dd00c3b36f8915cf55fc3c34e9c3dca90de
Mohamed Ouyizme <mohouyizme@Mohameds-MacBook-Pro.local>
2021-06-24 15:38:13 +0100

    Finish exercise 1

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("Daydreamer by Aurora")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("And we become night time dreamers")
+print("And street walkers, small talkers")
+print("When we should be daydreamers")
+print("And moonwalkers and dream talkers")
 /*:
 
  _Copyright © 2018 Apple Inc._
